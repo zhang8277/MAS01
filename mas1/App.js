7263a05b0af550e7122627af920baeb70acd084d
Zoetic-Zephyr <jack.zhang@nyu.edu>
2020-09-01 19:10:42 -0400

    Code refactoring for imports and self-closing tags

--- mas1/App.js
+++ mas1/App.js
@@ -1,35 +1,33 @@
-import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
-import Amplify from 'aws-amplify';
-import awsconfig from './aws-exports';
-import {Authenticator} from 'aws-amplify-react-native';
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+import Amplify from "aws-amplify";
+import awsconfig from "./aws-exports";
+import { Authenticator } from "aws-amplify-react-native";
 
 Amplify.configure(awsconfig);
 
 console.disableYellowBox = true;
 
-function Home(props){
-  if (props.authState === 'signedIn')
-  return <Text>Home</Text>
-  else return <></>
+function Home(props) {
+  if (props.authState === "signedIn") return <Text>Home</Text>;
+  else return <></>;
 }
 
-export default function App(){
+export default function App() {
   return (
-    <View style = {styles.container}>
+    <View style={styles.container}>
       <Authenticator usernameAttributes="email">
-      <Home></Home>
+        <Home />
       </Authenticator>
     </View>
-  )
+  );
 }
 
-const styles =  StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
-
-
 });
