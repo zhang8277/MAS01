e7e882a0949f704cd238670275f0d67cfb7e0c38
zhang8277 <zhang.8277@osu.edu>
2020-09-01 18:30:30 -0400

    01

---
+++ mas1/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+import {Authenticator} from 'aws-amplify-react-native';
+
+Amplify.configure(awsconfig);
+
+console.disableYellowBox = true;
+
+function Home(props){
+  if (props.authState === 'signedIn')
+  return <Text>Home</Text>
+  else return <></>
+}
+
+export default function App(){
+  return (
+    <View style = {styles.container}>
+      <Authenticator usernameAttributes="email">
+      <Home></Home>
+      </Authenticator>
+    </View>
+  )
+}
+
+const styles =  StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+
+});
