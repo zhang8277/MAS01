e2fdcfb50af81e21aaa782f777f6ca49e4f9f93a
J Z <47731591+zhang8277@users.noreply.github.com>
2020-09-01 23:19:43 -0400

    Update index.js

--- mas1/index.js
+++ mas1/index.js
@@ -2,8 +2,13 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { AppRegistry } from "react-native";
+import App from "./App";
+import { name as appName } from "./app.json";
+
+import Amplify from "aws-amplify";
+import awsconfig from "./aws-exports";
+
+Amplify.configure(awsconfig);
 
 AppRegistry.registerComponent(appName, () => App);
